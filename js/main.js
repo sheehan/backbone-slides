0b524fb53dbcd41974b1bc2ada506beff85cbd3e
Matt Sheehan <matt.sheehan@objectpartners.com>
2012-05-10 10:08:03 -0500

    playing with transitions

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 $(function () {
     $.deck('.slide');
-}());
+
+
+    var processSlide = function($slide) {
+        if ($slide.hasClass('vertical-code')) {
+            var $scroller = $slide.parent();
+            var scroll = $slide.position().top + $scroller.scrollTop() - 50;
+            $scroller.animate({scrollTop: scroll});
+        }
+    };
+
+    $(document).bind('deck.change', function (event, from, to) {
+        processSlide($.deck('getSlide', to));
+    });
+
+    processSlide($.deck('getSlide'));
+});
